8270012007249f57faeca6d82dbe258fab3caa68
vikassarkar <vikas.sarkar2014@gmail.com>
2018-03-04 19:26:24 +0530

    test

--- 2_react-router-ts-webpack/src/module/my-module/MyRouter.tsx
+++ 2_react-router-ts-webpack/src/module/my-module/MyRouter.tsx
@@ -6,21 +6,21 @@ import { Contact } from '../../pages/contact/Contact';
 import { About } from '../../pages/about/About';
 
 const configs:any = require("../../configs/env_config.json");
-
+const basePath = configs.githubBuild === "githubBuild"? configs.router_basename_build : configs.router_basename_dev
 export const MyRouter = () => {
   return (
-    <BrowserRouter basename={configs.githubBuild === "githubBuild"? configs.router_basename_build : configs.router_basename_dev}>
+    <BrowserRouter basename={basePath}>
       <div>
         <ul style={{ listStyle: "none", padding: "0" }}>
-          <li><Link to='/home'>Home</Link></li>
-          <li><Link to='/contact'>Contact</Link></li>
-          <li><Link to='/about'>About</Link></li>
+          <li><Link to={basePath+'/home'}>Home</Link></li>
+          <li><Link to={basePath+'/contact'}>Contact</Link></li>
+          <li><Link to={basePath+'/about'}>About</Link></li>
         </ul>
         <hr />
-        <Route exact path={configs.githubBuild === "githubBuild"? configs.router_basename_build : configs.router_basename_dev+'/'} component={Home} />
-        <Route path={configs.githubBuild === "githubBuild"? configs.router_basename_build : configs.router_basename_dev+'/home'} component={Home} />
-        <Route path={configs.githubBuild === "githubBuild"? configs.router_basename_build : configs.router_basename_dev+'/contact'} component={Contact} />
-        <Route path={configs.githubBuild === "githubBuild"? configs.router_basename_build : configs.router_basename_dev+'/about'} component={About} />
+        <Route exact path={basePath+'/'} component={Home} />
+        <Route path={basePath+'/home'} component={Home} />
+        <Route path={basePath+'/contact'} component={Contact} />
+        <Route path={basePath+'/about'} component={About} />
         {/* ways to add component in route - component, render, children */}
       </div>
     </BrowserRouter >
