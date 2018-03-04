5abb8b3d492073975497746ed90fc901b9f1d120
vikassarkar <vikas.sarkar2014@gmail.com>
2018-03-04 19:20:05 +0530

    test

--- 2_react-router-ts-webpack/src/module/my-module/MyRouter.tsx
+++ 2_react-router-ts-webpack/src/module/my-module/MyRouter.tsx
@@ -5,7 +5,7 @@ import { Home } from '../../pages/home/Home';
 import { Contact } from '../../pages/contact/Contact';
 import { About } from '../../pages/about/About';
 
-//const configs:any = require("../../configs/env_config.json");
+const configs:any = require("../../configs/env_config.json");
 
 export const MyRouter = () => {
   return (
@@ -17,10 +17,10 @@ export const MyRouter = () => {
           <li><Link to='/about'>About</Link></li>
         </ul>
         <hr />
-        <Route exact path='/' component={Home} />
-        <Route path='/home' component={Home} />
-        <Route path='/contact' component={Contact} />
-        <Route path='/about' component={About} />
+        <Route exact path={configs.githubBuild === "githubBuild"? configs.router_basename_build : configs.router_basename_dev+'/'} component={Home} />
+        <Route path={configs.githubBuild === "githubBuild"? configs.router_basename_build : configs.router_basename_dev+'/home'} component={Home} />
+        <Route path={configs.githubBuild === "githubBuild"? configs.router_basename_build : configs.router_basename_dev+'/contact'} component={Contact} />
+        <Route path={configs.githubBuild === "githubBuild"? configs.router_basename_build : configs.router_basename_dev+'/about'} component={About} />
         {/* ways to add component in route - component, render, children */}
       </div>
     </BrowserRouter >
